f050bd09cb3b46b800b9d437cd242d23b1e124c8
hasangzc <hasangzc2735@gmail.com>
2021-10-03 02:43:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
+
+
+
+print("You have completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
